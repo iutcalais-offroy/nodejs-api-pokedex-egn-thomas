1b6c58cdf3e78548e59118d93d2b013f9135c483
egn-thomas <thomas.eugene.62250@gmail.com>
2026-02-13 10:46:00 +0100

    style : format with prettier
    Format wiith prettier and check code quality with lint.
    
    Closes #1bis

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended'
+
+export default tseslint.config(
+  {
+    ignores: ['public/', 'dist/', 'node_modules/*', '.config.js'],
+  },
+
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettier,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'prettier/prettier': 'error',
+    },
+  },
+)
